93812ea61ae7a09ae99cd04faa772bc5aab6c07c
menmenmeng <franksmh0409@gmail.com>
2023-08-19 18:32:33 +0900

    updated train.py

--- source/train/train.py
+++ source/train/train.py
@@ -12,31 +12,27 @@ if __name__ == '__main__':
     penalty, default="l2"
     tol, default=1e-4
     C, default=1.0
-
     '''
+    
     parser.add_argument('--penalty', type=str, default="l2")
     parser.add_argument('--tol', type=float, default=1e-4)
-    parser.add_argument('--C', type=float, default=1.0)
-    parser.add_argument('--train_data_path', type=str, default="./data/output/train") # nb instance 내의 경로
-
-    parser.add_argument('--model-dir', type=str, default="./data/output/model") # nb instance 내 경로
-    parser.add_argument('--output-data-dir', type=str, default="./data/output") # nb instance 내 경로(이 파일에서 사용 X, cv 후 metric 저장을 위해)
+    parser.add_argument('--c_rate', type=float, default=1.0)
+    
+    parser.add_argument('--train_data_path', type=str, default=os.environ["SM_CHANNEL_TRAIN"])
+    parser.add_argument('--model-dir', type=str, default=os.environ["SM_MODEL_DIR"])
+    parser.add_argument('--output-data-dir', type=str, default=os.environ["SM_OUTPUT_DIR"])
 
     args = parser.parse_args()
 
-
-    ###########################################################################################
     # dataset load
-    ###########################################################################################
-
-    data = pd.read_csv(f"{args.train_data_path}/train.csv") # nb instance 내 경로
+    data = pd.read_csv(f"{args.train_data_path}/train.csv")
     train = data.drop('Survived', axis=1)
     label = pd.DataFrame(data['Survived'])
 
     params = {
         'penalty': args.penalty,
         'tol': args.tol,
-        'C': args.C
+        'C': args.c_rate
     }
 
 
