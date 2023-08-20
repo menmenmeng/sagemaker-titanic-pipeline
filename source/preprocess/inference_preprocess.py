75a2047346d3280ea72ba1803ea20b3a4fa4296a
menmenmeng <franksmh0409@gmail.com>
2023-08-21 01:28:37 +0900

    updated infer prep

--- source/preprocess/inference_preprocess.py
+++ source/preprocess/inference_preprocess.py
@@ -8,11 +8,66 @@ import boto3
 import pandas as pd
 import numpy as np
 from glob import glob
+from datetime import datetime
 
 from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
+
+
+def split_train_test(df, test_ratio=0.1):
+    '''
+    두 개의 데이터 세트로 분리
+    '''
+    total_rows = df.shape[0]
+    train_end = int(total_rows * (1 - test_ratio))
+    
+    train_df = df[0:train_end]
+    test_df = df[train_end:]
+    
+    return train_df, test_df
+
+
+def convert_type(raw, cols, type_target):
+    '''
+    해당 데이터 타입으로 변경
+    '''
+    df = raw.copy()
+    
+    for col in cols:
+        df[col] = df[col].astype(type_target)
+    
+    return df
+
+
+def drop_columns(raw, drop_cols):
+    '''
+    열을 제거
+    '''
+    df = raw.drop(drop_cols, axis=1)
+    return df
+
+
+def one_hot_encode_ONECOLUMN(raw, one_hot_col):
+    raw_ELSE = raw.drop([one_hot_col], axis=1)
+    raw_OH = pd.get_dummies(raw[one_hot_col])
+    df = pd.concat([raw_ELSE, raw_OH], axis=1)
+    return df
+
+
+def one_hot_encode_COLUMNS(raw, one_hot_cols):
+    for col in one_hot_cols:
+        raw = one_hot_encode_ONECOLUMN(raw, col)
+    return raw
+
+
+def get_today_string():
+    year = str(datetime.now().year)
+    month = str(datetime.now().month)
+    day = str(datetime.now().daty)
+    return year+month+day
+
 
 if __name__ == "__main__":
 
@@ -21,7 +76,7 @@ if __name__ == "__main__":
     ###########################################################################################
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--bucket_name', type=str, default="sagemaker-titanic-pipeline-bucket-0001")
+    # parser.add_argument('--bucket_name', type=str, default="sagemaker-titanic-pipeline-bucket-0001")
     parser.add_argument('--base_preproc_input_dir', type=str, default="/opt/ml/processing/input")
     parser.add_argument('--base_output_dir', type=str, default="/opt/ml/processing/output")
     parser.add_argument('--split_rate', type=float, default=0.15)
@@ -30,32 +85,21 @@ if __name__ == "__main__":
     # parse arguments
     args = parser.parse_args()
 
-    s3_bucket = args.bucket_name
+    # s3_bucket = args.bucket_name
     base_preproc_input_dir = args.base_preproc_input_dir
     base_output_dir = args.base_output_dir
     split_rate = args.split_rate
     # label_column = args.label_column
 
 
-    s3_client = boto3.client('s3')
-    current_directory = os.getcwd()
-
-    if current_directory not in sys.path:
-        sys.path.append(current_directory)
-
-    s3_client.download_file(s3_bucket, 'source/preprocess/preprocess.py', os.path.join(current_directory, 'preprocess.py'))
-    from preprocess import *
-
-
     # load titanic dataset
-    df = pd.read_csv(base_preproc_input_dir + '/raw_features.csv')
+    today = get_today_string()
+    df = pd.read_csv(f"{base_preproc_input_dir}/raw_{today}.csv")
 
     # 컬럼 종류
     id_col = ['PassengerId']
     drop_cols = ['Name', 'Age', 'Ticket', 'Cabin', 'Embarked', 'Fare']
     one_hot_cols = ['Sex']
-
-
 
     ###########################################################################################
     # preprocessing pipeline
@@ -85,4 +129,4 @@ if __name__ == "__main__":
     preprocessed_df = convert_type(preprocessed_df, preprocessed_df.columns, type_target='float')
     preprocessed_df = pd.concat([df_id, preprocessed_df], axis=1)
 
-    preprocessed_df.to_csv(f"{base_output_dir}/preprocessed/preprocessed.csv", index=False)
+    preprocessed_df.to_csv(f"{base_output_dir}/inference/prep_{today}.csv", index=False)
